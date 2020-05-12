490235013c1459bec5073cdb4506db4e646deed4
Matt Harding <mharding15@gmail.com>
2020-05-12 14:57:36 -0400

    More characters.
    Added the rest of the characters and animations.

---
+++ Assets/Scripts/Team1/Archer/ArcherMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcherMovement : MonoBehaviour
+{
+    public float speed, rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMovement();
+    }
+
+    void CheckMovement()
+    {
+        float move = Input.GetAxis("Vertical");
+        float rotate = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(0f, 0f, move * speed);
+        Vector3 rotation = new Vector3(0, rotate * rotationSpeed, 0);
+
+        transform.Translate(movement * Time.deltaTime);
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Team1/Brute/BruteAnimations.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruteAnimations : MonoBehaviour
+{
+    private Animator _animator;
+    private bool _isIdle, _isWalking, _isRunning, _isAttacking, _isDying, _isHit;
+    private int IDLE = 0, 
+                WALK = 1,
+                RUN = 2, 
+                ATTACK = 3,
+                DIE = 4,
+                HIT = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        SetAnimBools(IDLE);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)){
+            SetAnimBools(WALK);
+        } else if (Input.GetKey(KeyCode.W)){
+            SetAnimBools(RUN);
+        } else if (Input.GetKey(KeyCode.S)){
+            SetAnimBools(ATTACK);
+        // this will be if the character's HP goes to 0 in the actual game
+        } else if (Input.GetKey(KeyCode.D)){
+            SetAnimBools(DIE);
+        } else if (Input.GetKey(KeyCode.H)){
+            SetAnimBools(HIT);
+        } else {
+            SetAnimBools(IDLE);
+        }
+    }
+
+    void SetAnimBools(int state)
+    {
+        SetAllToFalse();
+
+        switch(state){
+            case 0:
+                _isIdle = true;
+                break;
+            case 1:
+                _isWalking = true;
+                break;
+            case 2:
+                _isRunning = true;
+                break;
+            case 3:
+                _isAttacking = true;
+                break;
+            case 4:
+                _isDying = true;
+                break;
+            case 5:
+                _isHit = true;
+                break;
+        }
+
+        _animator.SetBool("isIdle", _isIdle);
+        _animator.SetBool("isWalking", _isWalking);
+        _animator.SetBool("isRunning", _isRunning);
+        _animator.SetBool("isAttacking", _isAttacking);
+        _animator.SetBool("isDying", _isDying);
+        _animator.SetBool("isHit", _isHit);
+    }
+
+    void SetAllToFalse()
+    {
+        _isIdle = false;
+        _isWalking = false;
+        _isRunning = false;
+        _isAttacking = false;
+        _isDying = false;
+        _isHit = false;
+    }
+
+}
