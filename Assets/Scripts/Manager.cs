0a5ca18b505dfc2f6586e3359622fbaf15c97e99
Jesse <65253411+Jbird24@users.noreply.github.com>
2020-05-12 23:41:35 -0400

    basic grid generation

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+
+    //used for global variables and persistant behavior
+    
+    public static Manager Instance { get; private set; }
+
+    public int mapSizeX = 10;
+    public int mapSizeY = 10;
+
+
+
+    //makes sure there is only one Manager instance at any time 
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Map-grid/TileTerrain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class TileTerrain 
+{
+    public string terrain;
+    public int terrCode;
+    public int moveCost;
+
+    public GameObject visualPrefab;
+
+}
