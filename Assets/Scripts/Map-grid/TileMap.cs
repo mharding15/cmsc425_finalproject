eb81f6e51d23d90b0e5c078a75ac2c44e496b3fb
Jesse <65253411+Jbird24@users.noreply.github.com>
2020-05-13 13:33:04 -0400

    commented out unfixed syntax error
    Also pushing some change to the arrow that it insists I made? looks like it got moved and name changed, but I never did that manually. I'm hoping these changes will let me make a pull request of mhardings repo

--- Assets/Scripts/Map-grid/TileMap.cs
+++ Assets/Scripts/Map-grid/TileMap.cs
@@ -6,7 +6,7 @@ public class TileMap : MonoBehaviour
 {
     int[,] tiles;
     public TileTerrain[] tileTypes;
-    Random r = new Random();
+    //Random r = new Random();
 
     void Start()
     {
@@ -23,7 +23,7 @@ public class TileMap : MonoBehaviour
         {
             for (int y = 0; y < Manager.Instance.mapSizeY; y++)
             {
-                int terrain = r.Next(0,3); //add randomization here!!
+                int terrain = 0; //r.Next(0, 3); //add randomization here!!
                 tiles[x, y] = terrain;
                 Instantiate(tileTypes[terrain].visualPrefab, new Vector3(x,0,y), Quaternion.identity);
             }
