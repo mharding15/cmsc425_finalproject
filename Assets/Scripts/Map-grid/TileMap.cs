da5ad19d0bc6d03a827716aae1c6aecb54003725
Jesse <65253411+Jbird24@users.noreply.github.com>
2020-05-15 17:13:32 -0400

    base 'unit', input management interface, and small camera additions
    made a base Unit with a slightly raised child to make token based movement easier.
    began an imput management inheritence setup, having issues with this
    adden angle to the camera and a child empty for a foreward movement anchor

--- Assets/Scripts/Map-grid/TileMap.cs
+++ Assets/Scripts/Map-grid/TileMap.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class TileMap : MonoBehaviour
 {
+
+    
     int[,] tiles;
     public TileTerrain[] tileTypes;
     //Random r = new Random();
+    public GameObject selectedUnit;
 
     void Start()
     {
@@ -23,7 +26,7 @@ public class TileMap : MonoBehaviour
         {
             for (int y = 0; y < Manager.Instance.mapSizeY; y++)
             {
-                int terrain = 0; //r.Next(0, 3); //add randomization here!!
+                int terrain = 0; //r.Next(0, 3); //TODO add randomization here!!
                 tiles[x, y] = terrain;
                 Instantiate(tileTypes[terrain].visualPrefab, new Vector3(x,0,y), Quaternion.identity);
             }
