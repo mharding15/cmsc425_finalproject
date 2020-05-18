27b7106d578a4ad2f9928a7772bbe0b3766098b8
Jesse <65253411+Jbird24@users.noreply.github.com>
2020-05-17 22:10:18 -0400

    deleted some testing things

--- Assets/Scripts/Map-grid/TileClickable.cs
+++ Assets/Scripts/Map-grid/TileClickable.cs
@@ -19,49 +19,9 @@ public class TileClickable : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
-            //CombatLoop cl = GameObject.FindWithTag("Manager").GetComponent<CombatLoop>();
-            //cl.GetCurrentUnit.path =
+            CombatLoop cl = GameObject.FindWithTag("Manager").GetComponent<CombatLoop>();
+            cl.GetCurrentUnit.path = (new pathFinder(map, 3, 3, this)).solve();
 
-            otherCall();
-        } else
-        {
-            vate float _lastUpdateTime = 0f;
-
-            void Update()
-            {
-                if (Time.time - _lastUpdateTime >= 1.0f)
-                {
-                    _lastUpdateTime = Time.time;
-                    // TODO: Update your value
-                }
-            }
-            //map.selectedUnit.MoveTo(3, 3);
         }
-
-        
-    }
-
-    public bool isWalkable()
-    {
-        return (cost > 0);
-    }
-
-    public void otherCall()
-    {
-        List<Vector3> directions = (new pathFinder(map, 3, 3, this)).solve();
-        for (int i = 0; i < directions.Count; i++)
-        {
-            map.selectedUnit.MoveTo((int)directions[i].x, (int)directions[i].y);
-            float _lastUpdateTime = 0f;
-
-            while (Time.time - _lastUpdateTime < 1.0f)
-            {
-                _lastUpdateTime = Time.time;
-                    
-            }
-            
-        }
-    }
-
-    
+    }   
 }
